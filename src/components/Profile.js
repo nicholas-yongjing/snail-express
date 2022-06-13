c10a42a3c941e9f6f34d1d1a9517f34de7b50530
Yong Jing <gohyongjing@gmail.com>
2022-06-13 15:43:17 +0800

    Style profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+
+import NavigationBar from "./NavigationBar";
 
 export default function Profile() {
   const { currentUser } = useAuth();
+  const navigate = useNavigate();
+
   return (
-    <>
-      <Card>
-        <Card.Body>
-          {/* <img src={logo} className="img-responsive" alt="Website logo" height="150px" /> */}
-          <h2 className="text-center mb-4">Profile</h2>
+    <div>
+      <NavigationBar />
+      <div className="content profile">
+        {/* <img src={logo} className="img-responsive" alt="Website logo" height="150px" /> */}
+        <h1>Profile</h1>
+        <div className="info">
           <div>
             <strong>Name:</strong> Insert name here after linking database
           </div>
@@ -18,16 +22,16 @@ export default function Profile() {
             <strong>Email:</strong> {currentUser.email}
           </div>
           <div>
-            <strong>Enrolled course:</strong> CP2106  
+            <strong>Enrolled course:</strong> CP2106
           </div>
           <div>
             <strong>Role:</strong> Student
           </div>
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update profile
-          </Link>
-        </Card.Body>
-      </Card>
-    </>
+        </div>
+        <button className="generic-button" onClick={() => {navigate('/update-profile')}}>
+          Update profile
+        </button>
+      </div>
+    </div>
   );
 }
