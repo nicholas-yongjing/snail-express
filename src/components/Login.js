baa2ab98c3b51e44a44ec542b9a25d32cd6d6569
Yong Jing <gohyongjing@gmail.com>
2022-06-13 12:57:51 +0800

    Style log in page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,8 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 
+import NavigationBar from "./NavigationBar";
+
 export default function Login() {
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -27,93 +29,35 @@ export default function Login() {
   }
 
   return (
-    <>
-      <div className="container">
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-              snail-express
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                <a className="nav-link" href="/">
-                  Home
-                </a>
-                <a className="nav-link" href="#">
-                  Features
-                </a>
-                <a className="nav-link" href="#">
-                  Pricing
-                </a>
-                <a className="nav-link disabled"></a>
-              </div>
-            </div>
-            <nav className="navbar navbar-dark bg-dark">
-              <form className="form-inline">
-                <a
-                  href="/login"
-                  className="btn bg-primary text-white"
-                  role="button"
-                >
-                  Login
-                </a>
-                <span> </span>
-                <a
-                  href="/signup"
-                  className="btn btn-secondary text-white"
-                  role="button"
-                >
-                  Sign Up
-                </a>
-              </form>
-            </nav>
+    <div>
+      <NavigationBar />
+      <div className="log-in">
+        <h1>Login</h1>
+        {error && <div className="login-failed">{error}</div>}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label for='email'>Email Address: </label>
+            <input type='email' id='email' ref={emailRef} required></input>
           </div>
-        </nav>
-
-
-        <Card>
-          <Card.Body>
-            <h2 className="text-center mb-4">Login</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form onSubmit={handleSubmit}>
-              <Form.Group id="email">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" ref={emailRef} required />
-              </Form.Group>
-              <Form.Group id="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" ref={passwordRef} required />
-              </Form.Group>
-              <br></br>
-              <Button disabled={loading} className="w-100" type="submit">
-                Enter
-              </Button>
-            </Form>
-            <div className="w-100 text-center mt-3">
-              <Link to="/forgot-password">Forgot password?</Link>
-            </div>
-          </Card.Body>
-        </Card>
-        <br></br>
-        <div className="text-center">
-          New to our website? Create an account <Link to="/signup">here</Link>
-        </div>
-        <div className="text-center">
-          Back to <Link to="/">home</Link>
+          <div>
+            <label for='password'>Password: </label>
+            <input type='password' id='password' ref={passwordRef} required></input>
+          </div>
+          <button disabled={loading} type="submit">
+            Log in
+          </button>
+        </form>
+        <div className="log-in-footer">
+          <Link to="/forgot-password">Forgot password?</Link>
+          <div>
+            New to our website? Create an account <Link to="/signup">here</Link>
+          </div>
+          <div>
+            Back to <Link to="/">home</Link>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
