0ca3bab89e4cd537f8bcfa4f6c0f939589be9e86
Yong Jing <gohyongjing@gmail.com>
2022-06-22 15:53:16 +0800

    Refactor dashboard for teaching assistants

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -1,82 +1,41 @@
-import { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
 import { useClass } from '../contexts/ClassContext';
-import { getClasses } from '../database';
-import Invites from "./Invites";
 import { Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
-export default function Classes() {
-    const { currentUser } = useAuth();
-    const { setCurrentClass } = useClass();
-    const [enrolledClasses, setEnrolledClasses] = useState([]);
-    const [teachingClasses, setTeachingClasses] = useState([]);
-    const navigate = useNavigate();
+export default function Classes(props) {
+  const { setCurrentClass } = useClass();
+  const navigate = useNavigate();
+  const classType = props.classType;
+  const classes = props.classes;
 
-    useEffect(() => {
-        if (currentUser.email) {
-            populateClasses('student', setEnrolledClasses);
-            populateClasses('tutor', setTeachingClasses);
+  function handleClick(clss) {
+    setCurrentClass(clss)
+    navigate('/class-dashboard');
+  }
+
+  return (
+    <>
+        {
+          (classes.length > 0)
+            ? classes.map((clss) => {
+              return (
+                <Card key={clss.id}>
+                  <Card.Body>
+                    <h4
+                      key={clss.id}
+                      onClick={() => handleClick(clss)}
+                      className='d-flex align-items-center border-primary text-primary'
+                      style={{ cursor: 'pointer' }}
+                    >
+                      {clss.className}
+                    </h4>
+                    {classType} class
+                  </Card.Body>
+                </Card>
+              );
+            })
+            : <h4>No {classType} classes</h4>
         }
-    }, [currentUser]);
-
-    function populateClasses(userType, setter) {
-        getClasses(currentUser.uid, userType).then((classes) => {
-            if (classes) {
-                setter(classes);
-            }
-        })
-    }
-
-    function handleClick(clss) {
-        setCurrentClass(clss)
-        navigate('/class-dashboard');
-    }
-
-    return (
-        <Card>
-            <Card.Body>
-                <div className='d-flex gap-3 justify-content-between'>
-                    <h1>My classes</h1>
-                    <Link to="/add-class">
-                        <button type="button" className="btn btn-primary">
-                            Add Class
-                        </button>
-                    </Link>
-                </div>
-                <br />
-                {[['Enrolled', enrolledClasses],
-                ['Teaching', teachingClasses]].map(([header, arr]) => {
-                    return (
-                        <Card key={header}>
-                            <h2 className='d-flex justify-content-center p-2'>{header}</h2>
-                            <br />
-                              {
-                                (arr.length > 0) 
-                                ? arr.map((clss) => {
-                                    return (
-                                        <Card
-                                            key={clss.id} 
-                                            onClick={() => handleClick(clss)}
-                                            className='d-flex justify-content-center m-2 border-primary'
-                                            style={{cursor:'pointer'}}
-                                        >
-                                            <Card.Body>
-                                                <div>{clss.className}</div>
-                                            </Card.Body>
-                                        </Card>
-                                    );
-                                })
-                                : <h4 className='d-flex justify-content-center'>No classes</h4>
-                            } 
-                        </Card>
-                    );
-                })}
-            </Card.Body>
-            <Invites populateClasses={() => {
-                populateClasses('student', setEnrolledClasses)
-            }}/>
-        </Card>
-    );
+      </>
+  );
 };
--- src/database.js
+++ src/database.js
@@ -1,140 +1,183 @@
 import { collection, addDoc, query, getDocs, where, setDoc, getDoc, doc } from "firebase/firestore";
-import { auth, firestore } from "./firebase";
+import { firestore } from "./firebase";
 
-async function createClass(className, headTutor, studentsEmail) {
-    return await addDoc(collection(firestore, "classes"), {
-        className: className,
-        headTutor: headTutor,
-        invites: studentsEmail,
-        studentIds: []
-    }).catch((err) => {
-        console.log(`Error creating class: ${err}`);
-    })
+async function createClass(className, headTutor, studentsEmail, tutorsEmail) {
+  return await addDoc(collection(firestore, "classes"), {
+    className: className,
+    headTutor: headTutor,
+    studentInvites: studentsEmail,
+    tutorInvites: tutorsEmail,
+    studentIds: [],
+    tutorIds: []
+  }).catch((err) => {
+    console.log(`Error creating class: ${err}`);
+  })
 }
 
-async function addUserToClass(classId, user, userType) {
-    if (userType === 'student') {
-        const classDocRef = doc(firestore, "classes", classId);
-        getDoc(classDocRef)
-            .then((snapshot) => {
-                const oldStudentIds = snapshot.data().studentIds;
-                setDoc(classDocRef,
-                    { studentIds: [...oldStudentIds, user.id] },
-                    { merge: true })
-            });
-        const studentDocRef = doc(firestore, "classes", classId, "students", user.id);
-        return await setDoc(studentDocRef, user);
-    } else {
-        throw new Error(`Invalid user type: ${userType}`);
-    }
+async function addUserToClass(classId, user, role) {
+  async function addUserToArray() {
+    const field = role === 'student' ? 'studentIds' : 'tutorIds';
+    const classDocRef = doc(firestore, "classes", classId);
+    return await getDoc(classDocRef)
+      .then((snapshot) => {
+        const newData = {};
+        newData[field] = [...((snapshot.data())[field]), user.id]
+        setDoc(classDocRef, newData, { merge: true })
+      });  
+  }
+
+  async function addUserDoc() {
+    const field = role === 'student' ? 'students' : 'tutors';
+    const userDocRef = doc(firestore, "classes", classId, field, user.id);
+    return await setDoc(userDocRef, user);
+  }
+
+  if (role !== 'student' && role !== 'tutor') {
+    throw new Error(`Invalid user role: ${role}`);
+  }
+  return addUserToArray()
+    .then(() => addUserDoc())
+    .catch((err) => {
+      console.log(`Error adding user to class: ${err}`);
+    });
 }
 
 async function getClasses(userId, role) {
-    console.log("Retrieving classes")
-    let q;
-    if (role === 'student') {
-        q = query(collection(firestore, "classes"),
-            where('studentIds', 'array-contains', userId));
-    } else if (role === 'tutor') {
-        q = query(collection(firestore, "classes"),
-            where('headTutor.id', '==', userId));
-    } else {
-        throw new Error(`Unknown role: ${role}`)
-    }
-    return (getDocs(q)
-        .then((querySnapshot) => {
-            return querySnapshot.docs.map((docSnapshot) => {
-                return { ...(docSnapshot.data()), id: docSnapshot.id };
-            });
-        }).catch((err) => {
-            console.log(`Error retrieving classes: ${err}`)
-        }));
+  console.log("Retrieving classes")
+  let q;
+  if (role === 'student') {
+    q = query(collection(firestore, "classes"),
+      where('studentIds', 'array-contains', userId));
+  } else if (role === 'head tutor') {
+    q = query(collection(firestore, "classes"),
+      where('headTutor.id', '==', userId));
+  } else if (role === 'tutor') {
+    q = query(collection(firestore, "classes"),
+      where('tutorIds', 'array-contains', userId));
+  } else {
+    throw new Error(`Unknown role: ${role}`)
+  }
+  return (getDocs(q)
+    .then((querySnapshot) => {
+      return querySnapshot.docs.map((docSnapshot) => {
+        return { ...(docSnapshot.data()), id: docSnapshot.id };
+      });
+    }).catch((err) => {
+      console.log(`Error retrieving classes: ${err}`)
+    }));
 }
 
-async function getInvites(email) {
-    console.log("Retrieving invites")
-    const q = query(collection(firestore, "classes"),
-        where('invites', 'array-contains', email));
-    return (getDocs(q)
-        .then((querySnapshot) => {
-            return querySnapshot.docs.map((docSnapshot) => {
-                return { ...(docSnapshot.data()), id: docSnapshot.id };
-            });
-        }).catch((err) => {
-            console.log(`Error retrieving invites: ${err}`)
-        }));
+async function getInvites(email, role) {
+  console.log("Retrieving invites");
+  let field;
+  if (role === 'student') {
+    field = 'studentInvites';
+  } else if (role === 'tutor') {
+    field = 'tutorInvites';
+  } else {
+    throw new Error(`Invalid role for invitation type: ${role}`)
+  }
+  const q = query(collection(firestore, "classes"),
+    where(field, 'array-contains', email));
+  return (getDocs(q)
+    .then((querySnapshot) => {
+      return querySnapshot.docs.map((docSnapshot) => {
+        return { ...(docSnapshot.data()), id: docSnapshot.id };
+      });
+    }).catch((err) => {
+      console.log(`Error retrieving invites: ${err}`)
+    }));
 }
 
-async function deleteInvite(inviteId, email) {
-    const oldInvites = await getDoc(doc(firestore, "classes", inviteId));
-    const newInvites = oldInvites.data().invites.filter((invite) => invite !== email);
-    return await setDoc(doc(firestore, "classes", inviteId),
-        { invites: newInvites },
-        { merge: true });
+async function deleteInvite(inviteId, role, email) {
+  let field;
+  if (role === 'student') {
+    field = 'studentInvites';
+  } else if (role === 'tutor') {
+    field = 'tutorInvites';
+  } else {
+    throw new Error(`Invalid role for invitation type: ${role}`)
+  }
+  const clss = await getDoc(doc(firestore, "classes", inviteId));
+  const newData = {};
+  newData[field] = clss.data()[field].filter((invite) => invite !== email);
+  return await setDoc(doc(firestore, "classes", inviteId),
+    newData,
+    { merge: true });
 }
 
-async function acceptInvite(inviteId, studentId, email) {
-    const snapshot = await getDoc(doc(firestore, "classes", inviteId));
-    if (snapshot.data().invites.includes(email)) {
-        return addUserToClass(inviteId,
-            {
-                id: studentId,
-                email: email
-            },
-            'student')
-            .then(deleteInvite(inviteId, email));
-    } else {
-        throw new Error('User is not invited to class and cannot accept invite');
-    }
+async function acceptInvite(inviteId, studentId, role, email) {
+  let field;
+  if (role === 'student') {
+    field = 'studentInvites';
+  } else if (role === 'tutor') {
+    field = 'tutorInvites';
+  } else {
+    throw new Error(`Invalid role for invitation type: ${role}`)
+  }
+  const snapshot = await getDoc(doc(firestore, "classes", inviteId));
+  if (snapshot.data()[field].includes(email)) {
+    return addUserToClass(inviteId,
+      {
+        id: studentId,
+        email: email
+      },
+      role)
+      .then(deleteInvite(inviteId, role, email));
+  } else {
+    throw new Error('User is not invited to class and cannot accept invite');
+  }
 }
 
 async function addForumThread(classId, threadName) {
-    const threadsRef = collection(firestore, "classes", classId, "forumThreads");
-    return (
-        addDoc(threadsRef, { name: threadName })
-            .catch((err) => {
-                throw new Error(`Error adding forum thread: ${err}`);
-            })
-    );
+  const threadsRef = collection(firestore, "classes", classId, "forumThreads");
+  return (
+    addDoc(threadsRef, { name: threadName })
+      .catch((err) => {
+        throw new Error(`Error adding forum thread: ${err}`);
+      })
+  );
 }
 
 async function getForumThreads(classId) {
-    console.log("Retrieving forum threads");
-    const threadsRef = collection(firestore, "classes", classId, "forumThreads");
-    return (getDocs(threadsRef)
-        .then((querySnapshot) => {
-            return querySnapshot.docs.map((docSnapshot) => {
-                return { ...(docSnapshot.data()), id: docSnapshot.id };
-            });
-        }).catch((err) => {
-            throw new Error(`Error retrieving forum threads: ${err}`)
-        }));
+  console.log("Retrieving forum threads");
+  const threadsRef = collection(firestore, "classes", classId, "forumThreads");
+  return (getDocs(threadsRef)
+    .then((querySnapshot) => {
+      return querySnapshot.docs.map((docSnapshot) => {
+        return { ...(docSnapshot.data()), id: docSnapshot.id };
+      });
+    }).catch((err) => {
+      throw new Error(`Error retrieving forum threads: ${err}`)
+    }));
 }
 
 async function addForumPost(classId, threadId, postTitle, postBody, author) {
-    const post = {title: postTitle,
-                body: postBody,
-                author: author,
-                endorsed: null};
-    const postsRef = collection(firestore, "classes",
-        classId, "forumThreads", threadId, "forumPosts");
-    return await addDoc(postsRef, post).catch((err) => {
-        throw new Error(`Error creating post: ${err}`);
-    });    
+  const post = {
+    title: postTitle,
+    body: postBody,
+    author: author,
+    endorsed: null
+  };
+  const postsRef = collection(firestore, "classes",
+    classId, "forumThreads", threadId, "forumPosts");
+  return await addDoc(postsRef, post).catch((err) => {
+    throw new Error(`Error creating post: ${err}`);
+  });
 }
 
 async function getForumPosts(classId, threadId) {
-    console.log("Retrieving forum posts")
-    const postsRef = collection(firestore, "classes",
-        classId, "forumThreads", threadId, "forumPosts");
-    return (getDocs(postsRef)
-        .then((querySnapshot) => {
-            return querySnapshot.docs.map((docSnapshot) => {
-                return { ...(docSnapshot.data()), id: docSnapshot.id };
-            });
-        }).catch((err) => {
-            throw new Error(`Error retrieving forum posts: ${err}`)
-        })); 
+  console.log("Retrieving forum posts")
+  const postsRef = collection(firestore, "classes",
+    classId, "forumThreads", threadId, "forumPosts");
+  return (getDocs(postsRef)
+    .then((querySnapshot) => {
+      return querySnapshot.docs.map((docSnapshot) => {
+        return { ...(docSnapshot.data()), id: docSnapshot.id };
+      });
+    }).catch((err) => {
+      throw new Error(`Error retrieving forum posts: ${err}`)
+    }));
 }
 
 export { createClass, getInvites, acceptInvite, deleteInvite, getClasses, addForumThread, getForumThreads, addForumPost, getForumPosts };
