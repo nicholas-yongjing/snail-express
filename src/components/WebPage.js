17fc26040052a4b45a07f92328ee501fa34b25ae
Yong Jing <gohyongjing@gmail.com>
2022-06-28 17:31:49 +0800

    Fix forum sidebar

--- src/components/WebPage.js
+++ src/components/WebPage.js
@@ -3,9 +3,9 @@ import NavigationBar from "./NavigationBar";
 export default function WebPage(props) {
   const children = props.children;
   return (<div
-    className="vh-100  slate-800">
+    className="vh-100 slate-800 d-flex flex-column">
       <NavigationBar />
-      <div className="slate-800 d-flex flex-column">
+      <div className="flex-grow-1 slate-800 d-flex flex-column">
         {children}
       </div>
     </div>
