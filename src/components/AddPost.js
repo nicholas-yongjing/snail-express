5af398dbb0ccd6035c8262eb76c3aaa582597d81
Yong Jing <gohyongjing@gmail.com>
2022-06-30 15:21:52 +0800

    Fix try except finally clause

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -5,74 +5,75 @@ import { Alert, Button, Card, Form } from "react-bootstrap";
 import { useState, useRef } from "react";
 
 export default function AddPost(props) {
-    const { currentUser } = useAuth();
-    const { currentClass } = useClass();
-    const currentThread = props.currentThread;
-    const populatePosts = props.populatePosts;
-    const [error, setError] = useState('');
-    const [message, setMessage] = useState('');
-    const [loading, setLoading] = useState('');
-    const formRef = useRef();
-    const postTitleRef = useRef();
-    const postBodyRef = useRef();
+  const { currentUser } = useAuth();
+  const { currentClass } = useClass();
+  const currentThread = props.currentThread;
+  const populatePosts = props.populatePosts;
+  const [error, setError] = useState('');
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState('');
+  const formRef = useRef();
+  const postTitleRef = useRef();
+  const postBodyRef = useRef();
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        try {
-            setMessage('');
-            setError('');
-            setLoading(true);
-            addForumPost(currentClass.id,
-                currentThread.id,
-                postTitleRef.current.value,
-                postBodyRef.current.value,
-                {
-                    id: currentUser.uid,
-                    email: currentUser.email
-                });
-            formRef.current.reset();
-            setMessage('Post successfully created!')
-            populatePosts();
-        } catch {
-            setError('Failed to create post');
-        }
-        setLoading(false);
+  function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      setMessage('');
+      setError('');
+      setLoading(true);
+      addForumPost(currentClass.id,
+        currentThread.id,
+        postTitleRef.current.value,
+        postBodyRef.current.value,
+        {
+          id: currentUser.uid,
+          email: currentUser.email
+        });
+      formRef.current.reset();
+      setMessage('Post successfully created!')
+      populatePosts();
+    } catch {
+      setError('Failed to create post');
+    } finally {
+      setLoading(false);
     }
+  }
 
-    return (
-        <Card className="p-3 slate-700 text-slate-200">
-            <h2>Create Post</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            {message && <Alert variant="success">{message}</Alert>}
-            <Form
-                ref={formRef}
-                onSubmit={handleSubmit}
-                className="p-2 d-flex flex-column gap-3"
-            >
-                <Form.Group id="post-title">
-                    <Form.Control
-                        type="text"
-                        ref={postTitleRef}
-                        required 
-                        placeholder="Title"
-                        className="generic-field"
-                    />
-                </Form.Group>
-                <Form.Group id="post-body">
-                    <Form.Control
-                        as="textarea"
-                        rows={5}
-                        ref={postBodyRef}
-                        required
-                        placeholder="Post content"
-                        className="generic-field"
-                    />
-                </Form.Group>
-                <br></br>
-                <Button disabled={loading} className="w-25 btn btn-success" type="submit">
-                    Create Post
-                </Button>
-            </Form>
-        </Card>
-    );
+  return (
+    <Card className="p-3 slate-700 text-slate-200">
+      <h2>Create Post</h2>
+      {error && <Alert variant="danger">{error}</Alert>}
+      {message && <Alert variant="success">{message}</Alert>}
+      <Form
+        ref={formRef}
+        onSubmit={handleSubmit}
+        className="p-2 d-flex flex-column gap-3"
+      >
+        <Form.Group id="post-title">
+          <Form.Control
+            type="text"
+            ref={postTitleRef}
+            required
+            placeholder="Title"
+            className="generic-field"
+          />
+        </Form.Group>
+        <Form.Group id="post-body">
+          <Form.Control
+            as="textarea"
+            rows={5}
+            ref={postBodyRef}
+            required
+            placeholder="Post content"
+            className="generic-field"
+          />
+        </Form.Group>
+        <br></br>
+        <Button disabled={loading} className="w-25 btn btn-success" type="submit">
+          Create Post
+        </Button>
+      </Form>
+    </Card>
+  );
 }
--- src/components/AddReply.js
+++ src/components/AddReply.js
@@ -6,64 +6,65 @@ import { useState, useRef } from "react";
 import Button from "./Button";
 
 export default function AddReply(props) {
-    const { currentUser } = useAuth();
-    const { currentClass } = useClass();
-    const currentThread = props.currentThread;
-    const currentPost = props.currentPost;
-    const populatePosts = props.populatePosts;
-    const [error, setError] = useState('');
-    const [message, setMessage] = useState('');
-    const [loading, setLoading] = useState('');
-    const formRef = useRef();
-    const postBodyRef = useRef();
+  const { currentUser } = useAuth();
+  const { currentClass } = useClass();
+  const currentThread = props.currentThread;
+  const currentPost = props.currentPost;
+  const populatePosts = props.populatePosts;
+  const [error, setError] = useState('');
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState('');
+  const formRef = useRef();
+  const postBodyRef = useRef();
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        try {
-            setMessage('');
-            setError('');
-            setLoading(true);
-            addForumReply(currentClass.id,
-                currentThread.id,
-                currentPost.id,
-                postBodyRef.current.value,
-                {
-                    id: currentUser.uid,
-                    email: currentUser.email
-                });
-            formRef.current.reset();
-            setMessage('Reply successfully posted!')
-            populatePosts();
-        } catch {
-            setError('Failed to send reply');
-        }
-        setLoading(false);
+  function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      setMessage('');
+      setError('');
+      setLoading(true);
+      addForumReply(currentClass.id,
+        currentThread.id,
+        currentPost.id,
+        postBodyRef.current.value,
+        {
+          id: currentUser.uid,
+          email: currentUser.email
+        });
+      formRef.current.reset();
+      setMessage('Reply successfully posted!')
+      populatePosts();
+    } catch {
+      setError('Failed to send reply');
+    } finally {
+      setLoading(false);
     }
+  }
 
-    return (
-        <Card className="p-4 slate-800 text-slate-200">
-            <h2>Post Reply</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            {message && <Alert variant="success">{message}</Alert>}
-            <Form
-                ref={formRef}
-                onSubmit={handleSubmit}
-                className="d-flex flex-column gap-3"
-            >
-                <Form.Group id="post-body">
-                    <Form.Control
-                        as="textarea"
-                        rows={5}
-                        ref={postBodyRef}
-                        required
-                        placeholder="Reply"
-                        className="generic-field-light"
-                    />
-                </Form.Group>
-                <Button disabled={loading} className="w-25 create-button" type="submit">
-                    Create Reply
-                </Button>
-            </Form>
-        </Card>
-    );
+  return (
+    <Card className="p-4 slate-800 text-slate-200">
+      <h2>Post Reply</h2>
+      {error && <Alert variant="danger">{error}</Alert>}
+      {message && <Alert variant="success">{message}</Alert>}
+      <Form
+        ref={formRef}
+        onSubmit={handleSubmit}
+        className="d-flex flex-column gap-3"
+      >
+        <Form.Group id="post-body">
+          <Form.Control
+            as="textarea"
+            rows={5}
+            ref={postBodyRef}
+            required
+            placeholder="Reply"
+            className="generic-field-light"
+          />
+        </Form.Group>
+        <Button disabled={loading} className="w-25 create-button" type="submit">
+          Create Reply
+        </Button>
+      </Form>
+    </Card>
+  );
 }
