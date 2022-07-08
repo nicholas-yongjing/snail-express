9cf982d950a90e80ef9503cb02f2bf0066a9e6a7
nseah21 <nseah21@gmail.com>
2022-07-08 16:34:39 +0800

    Add logic for creating quiz

--- src/components/CreateQuiz.js
+++ src/components/CreateQuiz.js
@@ -1,16 +1,75 @@
-import React from "react";
-import { Card, Container } from "react-bootstrap";
+import React, { useEffect, useRef, useState } from "react";
+import Button from "../components/Button";
+import { Card, Container, Form, FormControl } from "react-bootstrap";
 import WebPage from "./WebPage";
 import Question from "./Question";
+import { firestore } from "../firebase";
+import { collection, setDoc, doc } from "firebase/firestore";
+import { useClass } from "../contexts/ClassContext";
 
 export default function CreateQuiz() {
+  const { currentClass } = useClass();
+  const quizNameRef = useRef();
+  const [addingQuestions, setAddingQuestions] = useState(false);
+
+  const handleCreateQuiz = () => {
+    const quizDocRef = doc(
+      firestore,
+      "classes",
+      currentClass.id,
+      "quizzes",
+      `${quizNameRef.current.value}`
+    );
+    setDoc(quizDocRef, {}).then(console.log("Setting quiz name"));
+    setAddingQuestions(true);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        handleCreateQuiz();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
     <WebPage>
-      <Container className="mt-3">
-        <Card className="slate-600 text-slate-200 fs-4">
-          <Question />
-        </Card>
-      </Container>
+      {addingQuestions ? (
+        <Container className="mt-3 d-flex flex-column">
+          <Card className="slate-600 text-slate-200 fs-4">
+            <div className="d-flex justify-content-center">
+              <div>
+                <Question name={quizNameRef.current.value} />
+              </div>
+            </div>
+          </Card>
+        </Container>
+      ) : (
+        <Container className="mt-3 d-flex flex-column">
+          <Card className="slate-600 text-slate-200 fs-4">
+            <div className="d-flex justify-content-center">
+              <div
+                className="slate-600 text-slate-200 fs-4 mt-3 d-flex flex-column justify-content-center"
+                style={{ maxWidth: "500px" }}
+              >
+                <Form.Label className="d-flex justify-content-center">
+                  Enter the title of your new quiz!
+                </Form.Label>
+                <FormControl ref={quizNameRef}></FormControl>
+                <Button onClick={handleCreateQuiz} className="mt-3 mb-3">
+                  Start
+                </Button>
+              </div>
+            </div>
+          </Card>
+        </Container>
+      )}
     </WebPage>
   );
 }
