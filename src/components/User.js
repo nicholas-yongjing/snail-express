85b438613acf094f1799bdb32ebb83de2373369e
Yong Jing <gohyongjing@gmail.com>
2022-07-16 16:52:53 +0800

    Abstract user info into component

---
+++ src/components/User.js
@@ -0,0 +1,67 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useClass } from '../contexts/ClassContext';
+import firestore from '../firestore';
+
+export default function User(props) {
+  const { currentClass } = useClass();
+  const { getLevellingSettings, getUser } = firestore;
+  const [user, setUser] = useState({});
+  const userId = props.userId;
+  const [levelUpExp, setlevelUpExp] = useState([]);
+  
+  const getUserGroup = useCallback(() => {
+    if (currentClass) {
+      if (currentClass.headTutor.id === userId) {
+        return 'headTutor';
+      } else if (currentClass.studentIds.includes(userId)) {
+        return 'students';
+      } else if (currentClass.tutorIds.includes(userId)) {
+        return 'tutors';
+      }
+    }
+  }, [currentClass]);
+  
+  const populateLevelUpExp = useCallback(() => {
+    if (currentClass) {
+      getLevellingSettings(currentClass.id)
+        .then((settings) => {
+          setlevelUpExp(settings.expRequirements)
+        })
+    }
+  }, [currentClass, getLevellingSettings]);
+
+  const populateUser = useCallback(() => {
+    if (userId && currentClass.id) {
+      const userGroup = getUserGroup();
+      if (userGroup === 'headTutor') {
+        setUser({ id: userId, name: currentClass.headTutor.name });
+      } else {
+        getUser(currentClass.id, userGroup, userId)
+          .then((user) => {
+            setUser(user);
+          }).then(() => {
+            populateLevelUpExp();
+          });
+      }
+    }
+  }, [userId, getUserGroup, populateLevelUpExp, getUser]);
+
+  useEffect(() => {
+    populateUser();
+  }, [populateUser]);
+
+  return (
+    <div className="slate-800">
+      <h2>
+        <strong>{user.name}</strong>
+      </h2>
+      <div>
+        {user.level !== undefined ? `Level: ${user.level}` : ''}
+      </div>
+      <div>
+        {user.exp !== undefined ? `EXP: ${user.exp} / ${levelUpExp[user.level]}` : ''}
+      </div>
+
+    </div>
+  );
+}
--- src/pages/ClassDashboard.js
+++ src/pages/ClassDashboard.js
@@ -1,60 +1,17 @@
-import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { useClass } from '../contexts/ClassContext';
-import firestore from '../firestore';
 import SideBar from "../components/SideBar"
 import WebPage from '../components/WebPage';
 import Header from '../components/Header';
+import User from '../components/User';
 
 export default function ClassDashBoard() {
   const { currentUser } = useAuth();
   const { currentClass, setCurrentClass } = useClass();
-  const { getLevellingSettings, getUser } = firestore;
-  const [classUser, setClassUser] = useState({});
-  const [levelUpExp, setlevelUpExp] = useState([]);
   const sidebarLinks = [['/students', 'Students'],
   ['/tutors', 'Tutors'],
   ['/settings', 'Settings']];
-
-  const populateLevelUpExp = useCallback(() => {
-    if (currentClass) {
-      getLevellingSettings(currentClass.id).then((settings) => {
-        setlevelUpExp(settings.expRequirements)
-      })
-    }
-  }, [currentClass, getLevellingSettings]);
-
-  const getRole = useCallback(() => {
-    if (currentUser && currentClass) {
-      if (currentClass.headTutor.id === currentUser.uid) {
-        return 'headTutor';
-      } else if (currentClass.studentIds.includes(currentUser.uid)) {
-        return 'student';
-      } else if (currentClass.tutorIds.includes(currentUser.uid)) {
-        return 'tutor';
-      }
-    }
-  }, [currentUser, currentClass]);
-
-  const populateClassUser = useCallback(() => {
-    if (currentUser.uid && currentClass.id) {
-      const role = getRole();
-      if (role === 'headTutor' || role === 'tutor') {
-        setClassUser({ id: currentUser.uid, name: currentUser.displayName });
-      } else if (role === 'student') {
-        getUser(currentClass.id, 'students', currentUser.uid).then((user) => {
-          setClassUser(user);
-        }).then(() => {
-          populateLevelUpExp();
-        });
-      }
-    }
-  }, [currentUser.uid, currentUser.displayName, currentClass.id, getRole, populateLevelUpExp, getUser]);
-
-  useEffect(() => {
-    populateClassUser();
-  }, [populateClassUser]);
 
   function handleClick() {
     setCurrentClass(null);
@@ -86,18 +43,9 @@ export default function ClassDashBoard() {
             handleClick={handleClick}
             buttonClass="light-button"
           />
-          <h2>
-            <strong>{classUser.name}</strong>
-          </h2>
-          <div>
-            {classUser.level !== undefined ? `Level: ${classUser.level}` : ''}
-          </div>
-          <div>
-            {classUser.exp !== undefined ? `EXP: ${classUser.exp} / ${levelUpExp[classUser.level]}` : ''}
-          </div>
+          <User userId={currentUser.uid} />
         </div>
       </div>
     </WebPage>
-
   );
 }
