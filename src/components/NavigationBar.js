d09de5ee19bcb6b4f48dc1fcd6d6c5c7b39e36b3
nseah21 <nseah21@gmail.com>
2022-07-22 20:17:26 +0800

    Fix compilation warnings

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import Button from "../components/Button";
 import { useClass } from "../contexts/ClassContext";
 import SnailLogo from "../images/snail-logo.png";
 
