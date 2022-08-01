88cb15ded81f22372f52defe5dfc94a31a2b9eac
Yong Jing <gohyongjing@gmail.com>
2022-08-01 17:56:29 +0800

    Add logos to class dashboard

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,7 +3,7 @@ export default function SideBar(props) {
     <div
       data-testid="sidebar"
       className='d-flex flex-column align-items-stretch gap-3 p-4 fs-4 slate-700'
-      style={{ width: 'min(400px, 25vw)'}}
+      style={{ width: 'max(min(400px, 25vw), 400px)'}}
     >
 
       {props.children}
